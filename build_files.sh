362c1206bae0f81d29fa6a71c4ca21bdeea32944
Rahul Shahu <109348376+Rahulshahu340@users.noreply.github.com>
2023-09-01 18:42:17 +0530

    Update build_files.sh
    final commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
-python3.11 manage.py collectstatic
+#!/bin/bash
+
+echo "Building the project"
+python3.11 pip install  -r requirement.txt
+echo "Make Migration..."
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
+
+echo 'collect static...'
+python3.11 manage.py collect --noinput --clear
